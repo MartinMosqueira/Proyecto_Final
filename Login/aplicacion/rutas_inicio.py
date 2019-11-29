de1c931fa2645b98439857012f38bbfd9512519f
ralvarezum <rorototi@hotmail.com>
2019-11-29 00:49:33 -0300

    nuevo html

--- Login/aplicacion/rutas_inicio.py
+++
@@ -1,6 +0,0 @@
-from flask import request, render_template, make_response
-from flask import current_app as app
-
-@app.route("/", methods=["GET"])
-def pagina_principal():
-    return render_template("formulario.html", titulo="Formulario")
